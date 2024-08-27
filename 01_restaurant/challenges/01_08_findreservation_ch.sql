9898e3247f45214ad93485f86fd03cd9a9ca718a
virag-nemeth <153287323+virag-nemeth@users.noreply.github.com>
2024-08-27 03:30:20 +0000

    Challege_08

--- 01_restaurant/challenges/01_08_findreservation_ch.sql
+++ 01_restaurant/challenges/01_08_findreservation_ch.sql
@@ -5,3 +5,15 @@
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 
 -- There are four people in the party. Today is June 14th.
+
+SELECT 
+  R.CustomerID,
+  C.FirstName,
+  C.LastName,
+  R.PartySize,
+  R.Date
+FROM Reservations AS R
+JOIN Customers As C ON C.CustomerID = R.CustomerID
+Where C.LastName Like 'St%'
+    And R.PartySize = 4
+ORDER BY R.Date DESC;
