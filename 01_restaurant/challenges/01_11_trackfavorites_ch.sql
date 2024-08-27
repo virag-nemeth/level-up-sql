754823d0374cef49f9e6b5f86ad1d0e30ce1b703
virag-nemeth <153287323+virag-nemeth@users.noreply.github.com>
2024-08-27 07:13:51 +0000

    Challenge_11

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,27 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+SELECT DishID
+FROM Dishes
+Where name = 'Quinoa Salmon Salad';
+
+SELECT
+  FirstName,
+  LastName,
+  FavoriteDish
+FROM Customers
+Where FirstName = 'Cleo' AND LastName = 'Goldwater'
+
+Update Customers
+  Set FavoriteDish = (SELECT DishID FROM Dishes Where name = 'Quinoa Salmon Salad')
+  Where FirstName = 'Cleo' AND LastName = 'Goldwater';
+
+SELECT 
+  C.FirstName, 
+  C.LastName,
+  C.FavoriteDish, 
+  D.Name 
+FROM Customers AS C
+JOIN Dishes AS D ON C.FavoriteDish = D.DishID
+Where FirstName = 'Cleo' AND LastName = 'Goldwater';
