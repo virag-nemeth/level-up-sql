ba377d4f54038a8d0e381febecb3b01d6f745c2e
virag-nemeth <153287323+virag-nemeth@users.noreply.github.com>
2024-08-27 07:24:00 +0000

    Challenge_12

--- 01_restaurant/challenges/01_12_bestcustomers_ch.sql
+++ 01_restaurant/challenges/01_12_bestcustomers_ch.sql
@@ -1,3 +1,14 @@
 -- Identify a few customers who have ordered delivery
 -- from the restaurant the most often, so we can send
 -- them a promotional coupon.
+
+Select
+  C.FirstName,
+  C.LastName,
+  Count(O.OrderID) AS OrderCount,
+  C.Email
+FROM Orders AS O
+JOIN Customers AS C ON O.CustomerID = C.CustomerID
+group BY O.CustomerID
+Order by OrderCount DESC
+Limit 15;
