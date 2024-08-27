95a6273b7c25c3d8be788c07ccf439f3ee33d324
virag-nemeth <153287323+virag-nemeth@users.noreply.github.com>
2024-08-27 02:46:36 +0000

    Challenge_04

--- 01_restaurant/challenges/01_04_newcustomer_ch.sql
+++ 01_restaurant/challenges/01_04_newcustomer_ch.sql
@@ -4,3 +4,16 @@
 -- Anna Smith (asmith@samoca.org)
 -- 479 Lapis Dr., Memphis, TN
 -- Phone: (555) 555-1212; Birthday: July 21, 1973
+
+INSERT INTO Customers
+  (FirstName, LastName, Email, Address, City, State, Phone, Birthday)
+VALUES ('Anna', 'Smith', 'asmith@samoca.org', '479 Lapis Dr.', 'Memphis', 'TN', '(555) 555-1212', 'July 21, 1973');
+
+SELECT * 
+FROM Customers 
+ORDER BY CustomerID DESC;
+
+Update Customers
+SET Birthday = '1973-07-21'
+Where CustomerID = 101;
+
