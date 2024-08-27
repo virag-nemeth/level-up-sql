802b4b62e7701b6b33ec39a7723610110abffa4e
virag-nemeth <153287323+virag-nemeth@users.noreply.github.com>
2024-08-27 03:05:21 +0000

    Challenge_06

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -3,3 +3,10 @@
 -- A customer named Norby has notified us he won't 
 -- be able to keep his Friday reservation. 
 -- Today is July 24, 2022.
+
+SELECT *
+FROM Reservations AS R
+JOIN Customers AS C ON R.CustomerID = C.CustomerID
+WHERE C.FirstName = 'Norby' And R.Date > '2022-07-24';
+
+DELETE FROM Reservations WHERE ReservationID = 2000;
