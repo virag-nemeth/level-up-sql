1d0543a32462bf4b492dce468193efe9b9d6cc67
virag-nemeth <153287323+virag-nemeth@users.noreply.github.com>
2024-08-27 06:57:41 +0000

    Challenge_10

--- 01_restaurant/challenges/01_10_takeorder_ch.sql
+++ 01_restaurant/challenges/01_10_takeorder_ch.sql
@@ -7,3 +7,56 @@
 -- 1 Tropical Blue Smoothie
 -- Delivery date and time: September 20, 2022 @ 2PM (14:00)
 -- There are no taxes or other fees.
+
+-- Nested Subquery for CustomerID 
+SELECT
+  CustomerID
+FROM Customers
+WHERE LastName = 'Hundey' And Address = '6939 Elka Place';
+
+-- Creating the order
+INSERT into Orders
+  (CustomerID, OrderDate)
+VALUES (
+  (SELECT CustomerID
+    FROM Customers
+    WHERE LastName = 'Hundey' And Address = '6939 Elka Place'),
+  '2022-09-20 14:00');
+
+-- Checking the order was created
+Select *
+FROM Orders
+Where CustomerID = (SELECT CustomerID
+    FROM Customers
+    WHERE LastName = 'Hundey' And Address = '6939 Elka Place')
+ORDER BY OrderDate DESC;
+
+-- Correcting the timestamp
+Update Orders
+Set OrderDate = '2022-09-20 14:00:00'
+Where OrderID = 1001;
+
+-- Adding the ordered dishes to the OrderDish table
+INSERT into OrdersDishes
+  (OrderID, DishID)
+VALUES 
+  (1001, (Select DishID FROM dishes Where Name = 'House Salad')),
+  (1001, (Select DishID FROM dishes Where Name = 'Mini Cheeseburgers')),
+  (1001, (Select DishID FROM dishes Where Name = 'Tropical Blue Smoothie'));
+
+--Checking the oder deatils were added
+Select *
+FROM OrdersDishes
+Order By OrderID DESC;
+
+SELECT * 
+FROM Dishes 
+JOIN OrdersDishes ON Dishes.DishID = OrdersDishes.DishID 
+WHERE OrdersDishes.OrderID = 1001;
+
+-- Total sum of the order
+Select
+  SUM(D.Price)
+FROM Dishes AS D
+Join OrdersDishes AS OD ON D.DishID = OD.DishID
+Where OD.OrderID = 1001;
