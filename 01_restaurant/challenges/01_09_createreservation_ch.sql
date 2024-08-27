8443f87ef62aff945b6afaf27c3c9496524bbb4b
virag-nemeth <153287323+virag-nemeth@users.noreply.github.com>
2024-08-27 06:15:39 +0000

    Challenge_09

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,29 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+Select *
+FROM Customers
+Where Email = 'smac@kinetecoinc.com';
+
+INSERT INTO Customers
+  (FirstName, LastName, Email)
+  VALUES ('Sam', 'McAdams', 'smac@kinetecoinc.com');
+
+INSERT INTO Reservations
+  (CustomerID, Date, PartySize)
+  VALUES (
+    (Select CustomerID 
+      FROM Customers 
+      Where Email = 'smac@kinetecoinc.com'),
+      '2022-08-12 18:00', 5);
+
+Select
+  C.FirstName,
+  C.LastName,
+  C.Email,
+  R.Date,
+  R.PartySize
+FROM Customers AS C
+JOIN Reservations AS R ON C.CustomerID = R.CustomerID
+Where C.Email = 'smac@kinetecoinc.com';
