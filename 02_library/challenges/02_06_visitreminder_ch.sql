88ff69b7bf2b264a7a94a1065b36c31c2c3dbf54
virag-nemeth <153287323+virag-nemeth@users.noreply.github.com>
2024-08-28 03:08:21 +0000

    Challenge_02_06

--- 02_library/challenges/02_06_visitreminder_ch.sql
+++ 02_library/challenges/02_06_visitreminder_ch.sql
@@ -1,2 +1,13 @@
 -- Prepare a report of the library patrons
 -- who have checked out the fewest books.
+
+SELECT
+  P.FirstName,
+  P.LastName,
+  P.Email,
+  Count(L.LoanID) AS LoanCount
+FROM Loans AS L
+JOIN Patrons AS P ON L.PatronID = P.PatronID
+Group BY P.PatronID
+Order BY LoanCount
+Limit 9;
