7b99d7576f6d3272ff6fe9b40b5419b99e2ed0a5
virag-nemeth <153287323+virag-nemeth@users.noreply.github.com>
2024-08-28 03:32:23 +0000

    Challenge_02_08

--- 02_library/challenges/02_08_bookstats_ch.sql
+++ 02_library/challenges/02_08_bookstats_ch.sql
@@ -5,3 +5,21 @@
 
 -- Report 2: Show the five books that have been
 -- checked out the most.
+
+--Report '1
+SELECT
+  Published,
+  Count(DISTINCT(Title)) AS TitleCount
+FROM Books
+Group BY Published
+ORDER BY TitleCount DESC;
+
+-- Report 2
+SELECT
+  B.Title,
+  Count(L.LoanID) AS LoanCount
+From Loans AS L
+Join Books AS B ON L.BookID = B.BookID
+Group BY B.Title
+Order BY LoanCount DESC
+Limit 5;
