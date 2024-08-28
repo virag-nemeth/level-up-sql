01294211078ad833348c4e5833e9df2d2d3c57fb
virag-nemeth <153287323+virag-nemeth@users.noreply.github.com>
2024-08-28 02:40:46 +0000

    Challenge_02_04

--- 02_library/challenges/02_04_booksdue_ch.sql
+++ 02_library/challenges/02_04_booksdue_ch.sql
@@ -2,3 +2,15 @@
 -- to the library on July 13, 2022.
 -- Provide the due date, the book title, and
 -- the borrower's first name and email address.
+
+
+SELECT
+  L.DueDate,
+  B.Title,
+  P.FirstName,
+  P.Email
+FROM Loans AS L
+JOIN Books AS B ON L.BookID = B.BookID
+JOIN Patrons AS P ON L.PatronID = P.PatronID
+WHERE L.DueDate = '2022-07-13'
+  AND L.ReturnedDate IS NULL;
